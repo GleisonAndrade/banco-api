95d8b6c512eb2b0a038ab1e21e4044827efcbbf2
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2019-04-05 09:40:04 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/br/com/gleisonandrade/bancoapi/BancoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gleisonandrade.bancoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gleisonandrade/bancoapi/BancoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.gleisonandrade.bancoapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BancoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
