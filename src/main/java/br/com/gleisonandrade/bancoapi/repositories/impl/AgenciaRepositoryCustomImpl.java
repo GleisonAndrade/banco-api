c877429d8253b97066b154cdb5c5b3399cef1caa
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2019-04-07 01:33:17 -0300

    Refatoração da camada de persistência.
    Criação de um pacote '/repositories/impl' mantém uma classe que
    implementa os métodos persosnalizados para cada entidade;

---
+++ src/main/java/br/com/gleisonandrade/bancoapi/repositories/impl/AgenciaRepositoryCustomImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.gleisonandrade.bancoapi.repositories.impl;
+
+import br.com.gleisonandrade.bancoapi.domain.Agencia;
+import br.com.gleisonandrade.bancoapi.repositories.custom.AgenciaRepositoryCustom;
+
+/**
+ * @author <a href="malito:gleisondeandradeesilva@gmail.com">Gleison Andrade</a>
+ *
+ */
+public class AgenciaRepositoryCustomImpl implements AgenciaRepositoryCustom{
+
+	@Override
+	public Agencia buscarPorNumero(String numero) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
