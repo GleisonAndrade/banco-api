72be05bf57ac74de8519361cbb8c39ab2ef46e1a
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2019-04-08 02:57:28 -0300

    Adicionando a busca de cliente por CPF no ClienteService.

--- src/main/java/br/com/gleisonandrade/bancoapi/services/ClienteService.java
+++ src/main/java/br/com/gleisonandrade/bancoapi/services/ClienteService.java
@@ -3,22 +3,25 @@
  */
 package br.com.gleisonandrade.bancoapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.gleisonandrade.bancoapi.domain.Cliente;
 import br.com.gleisonandrade.bancoapi.repositories.ClienteRepository;
+import br.com.gleisonandrade.bancoapi.services.exceptions.ObjetoNaoEncontradoException;
 
 /**
  * @author <a href="malito:gleisondeandradeesilva@gmail.com">Gleison Andrade</a>
  *
  */
 @Service
-public class ClienteService extends GenericServiceImpl<Cliente, Long>{
-	
+public class ClienteService extends GenericServiceImpl<Cliente, Long> {
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	public ClienteService(ClienteRepository clienteRepository) {
 		super(clienteRepository);
 		this.clienteRepository = clienteRepository;
@@ -28,6 +31,13 @@ public class ClienteService extends GenericServiceImpl<Cliente, Long>{
 	protected Cliente atualizaDados(Cliente entity, Cliente newEntity) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Cliente buscarPorCpf(String cpf) {
+		Optional<Cliente> cliente = clienteRepository.buscarPorCpf(cpf);
+
+		return cliente.orElseThrow(() -> new ObjetoNaoEncontradoException(
+				"Cliente não encontrado! CPF: " + cpf));
 	}
 
 }
