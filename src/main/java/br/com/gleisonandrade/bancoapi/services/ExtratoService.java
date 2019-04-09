4ad59f9cb90f353242954ab334af4658395655bc
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2019-04-08 21:03:37 -0300

    Adicionando operação de extrato de conta do tipo SAQUE.

--- src/main/java/br/com/gleisonandrade/bancoapi/services/ExtratoService.java
+++ src/main/java/br/com/gleisonandrade/bancoapi/services/ExtratoService.java
@@ -3,11 +3,16 @@
  */
 package br.com.gleisonandrade.bancoapi.services;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.gleisonandrade.bancoapi.domain.Conta;
 import br.com.gleisonandrade.bancoapi.domain.Extrato;
+import br.com.gleisonandrade.bancoapi.domain.enuns.TipoOperacao;
 import br.com.gleisonandrade.bancoapi.repositories.ExtratoRepository;
+import br.com.gleisonandrade.bancoapi.services.exceptions.NegocioException;
 
 /**
  * @author <a href="malito:gleisondeandradeesilva@gmail.com">Gleison Andrade</a>
@@ -27,5 +32,15 @@ public class ExtratoService extends GenericServiceImpl<Extrato, Long>{
 	protected Extrato atualizaDados(Extrato entity, Extrato newEntity) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public Extrato gerar(Conta conta, TipoOperacao tipo, Double valor) {
+		if(TipoOperacao.SAQUE.equals(tipo)) {
+			Extrato extrato = new Extrato(Calendar.getInstance(), tipo, valor, conta);
+			extrato = extratoRepository.save(extrato);
+			return extrato;
+		}
+		
+		throw new NegocioException("Não foi possível gerar um extrato!");
 	}
 }
