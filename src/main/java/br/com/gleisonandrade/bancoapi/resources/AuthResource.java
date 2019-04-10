6a695eeba3be96aa92e26ad859e2da5cd38a6d84
Gleison Andrade <gleisondeandradeesilva@gmail.com>
2019-04-10 01:08:19 -0300

    Adicionando refresh token.

---
+++ src/main/java/br/com/gleisonandrade/bancoapi/resources/AuthResource.java
@@ -0,0 +1,32 @@
+package br.com.gleisonandrade.bancoapi.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.gleisonandrade.bancoapi.security.UserDetailsImpl;
+import br.com.gleisonandrade.bancoapi.services.UserService;
+import br.com.gleisonandrade.bancoapi.util.JWTUtil;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserDetailsImpl user = UserService.autenticar();
+		
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		
+		return ResponseEntity.noContent().build();
+	}
+}
